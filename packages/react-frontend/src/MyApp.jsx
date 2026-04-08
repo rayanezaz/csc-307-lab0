0e803dc5a7bc52b635b273eb457d6e91e59a524c
rayanezaz <rezaz@calpoly.edu>
2026-04-07 22:05:53 -0700

    Completed step with form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,10 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspiring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -30,10 +14,11 @@ function MyApp() {
     }
 
   return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-    </div>
-  );
+  <div className="container">
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    <Form />
+  </div>
+);
 }
 
 export default MyApp;
