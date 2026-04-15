2abe34317bf3ec6822ee008c08a2b53c4d5ef4f9
rayanezaz <rezaz@calpoly.edu>
2026-04-15 16:09:12 -0700

    Add delete user endpoint and filter users by name and job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -27,20 +27,36 @@ const addUser = (user) => {
     return user;
 }
 
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex((user) => user["id"] === id);
+
+  if (index === -1) {
+    return false;
+  }
+
+  users["users_list"].splice(index, 1);
+  return true;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
+  const job = req.query.job;
+
+  let result = users["users_list"];
 
   if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
+    result = result.filter((user) => user["name"] === name);
   }
+
+  if (job != undefined) {
+    result = result.filter((user) => user["job"] === job);
+  }
+
+  res.send({ users_list: result });
 });
 
 app.get("/users/:id", (req, res) => {
@@ -58,6 +74,17 @@ app.post("/users", (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
     res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  const deleted = deleteUserById(id);
+
+  if (!deleted) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send();
+  }
 });
 
 app.listen(port, () => {
