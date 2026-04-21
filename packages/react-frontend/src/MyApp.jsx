8bebf4f23561e45d7d58d97fd3284ad92fccc11d
rayanezaz <rezaz@calpoly.edu>
2026-04-21 16:22:19 -0700

    Linked frontend and backend so POST works and updates backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -20,6 +20,18 @@ function MyApp() {
       });
   }, []);
 
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -35,7 +47,11 @@ function MyApp() {
 );
 
 function updateList(person) {
-  setCharacters([...characters, person]);
+  postUser (person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
 }
 }
 
